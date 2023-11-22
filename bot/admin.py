fff6e7a4f0e3a2beff26a363dc9e616fd8c80f76
JohnVincentRamada <89288664+JohnVincentRamada@users.noreply.github.com>
2023-11-22 10:35:36 +0800

    Delete bot directory
    delete

--- bot/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from bot.models import RequestContext
-
-class RequestContextAdmin(admin.ModelAdmin):
-    list_display = ('id','request', 'comment', 'status', 'created_at') 
-
-admin.site.register(RequestContext, RequestContextAdmin)
