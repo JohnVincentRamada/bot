3c06b458d82c2dbdc4ce180da67536d793ea1543
JohnVincentRamada <89288664+JohnVincentRamada@users.noreply.github.com>
2023-11-22 10:35:53 +0800

    Delete customadmin directory
    delete

--- customadmin/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from customadmin.models import Description
-
-class DescriptionAdmin(admin.ModelAdmin):
-    list_display = ('id','title', 'category', 'answer', 'created_at') 
-
-admin.site.register(Description, DescriptionAdmin)
